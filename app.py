b1fea6d059ba12940af1da1d11b3d141e8a0f9c6
upworka0 <upworka0@gmail.com>
2020-07-22 09:56:27 -0700

    init

---
+++ app.py
@@ -0,0 +1,62 @@
+# python3
+"""
+    Python Flask Rest api to reflect AWS api gateway
+    """
+
+from flask import Flask, request, render_template, redirect, flash
+import requests
+import os
+import json
+
+app = Flask(__name__)
+app.secret_key = 'some secret key'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_FILE = "%s/.env" % BASE_DIR
+
+
+# retrieve url from env file
+def get_api_url():
+    file = open(ENV_FILE, "r")
+    return file.read()
+
+
+# store url to env file
+def store_url(_url):
+    file = open(ENV_FILE, "w")
+    file.write(_url)
+    file.close()
+
+
+@app.route("/", methods=['POST'])
+def index():
+    """
+        Reflector function
+        """
+
+    request_url = get_api_url()
+    data = request.get_json()
+
+    res = requests.post(request_url, json.dumps(data))
+    message = ""
+    if res.status_code != 200:
+        message = res.text
+    return message, res.status_code
+
+
+@app.route('/url', methods=['GET', 'POST'])
+def url():
+    """
+        Form to edit api gateway url
+        """
+    if request.method == 'POST':
+        # store url to temp file
+        store_url(request.form['url'])
+        flash('Successfully updated.', 'success')
+        return redirect('/url')
+
+    return render_template('form.html', url=get_api_url())
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
